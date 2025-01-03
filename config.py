16f9d00bbdf0b143d57e5f08fc92e84a110f7e28
WinxXD <167777352+WinxXD@users.noreply.github.com>
2025-01-03 10:37:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ OWNER = os.environ.get("OWNER", "ＦＯＲＳムＫＥＮ")
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "1494667620"))
 #Port
-PORT = os.environ.get("PORT", "8030")
+PORT = os.environ.get("PORT", "8080")
 #Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ItsParadoxical:zhsXK3Yfk2SkGVrI@cluster0.agsr3.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
